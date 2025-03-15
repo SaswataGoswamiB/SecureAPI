dab073d72dd1f6551c76eeb7f5ec59cd18c912c0
SaswataGoswami <85555421+SaswataGoswami@users.noreply.github.com>
2025-03-15 23:01:15 +0530

    Adding the CSRF file and Custom Credentials

---
+++ SpringSecurity/src/main/java/com/securityImpl/security/Controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.securityImpl.security.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Product")
+public class ProductController {
+
+     List<Product> products = new ArrayList<>(List.of(
+            new Product(1,"Iphone",
+                    99000.0),
+            new Product(2,"mac",
+                    199000.0),
+            new Product(3,"Fridge",
+                    55648.0)
+    )
+    );
+
+    private record Product(Integer productId,String productname,double price)
+    {}
+
+    @GetMapping
+    public List<Product> getprpducts()
+    {
+        return products;
+    }
+
+    @PostMapping
+    public Product SaveProduct(@RequestBody Product product)
+    {
+        products.add(product);
+        return product;
+
+    }
+}
