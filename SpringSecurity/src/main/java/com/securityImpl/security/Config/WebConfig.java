f37e7119de6df7aa736a846de60c4c417f6f2ba4
SaswataGoswami <85555421+SaswataGoswami@users.noreply.github.com>
2025-03-16 18:28:13 +0530

    MultiUser Code

---
+++ SpringSecurity/src/main/java/com/securityImpl/security/Config/WebConfig.java
@@ -0,0 +1,47 @@
+package com.securityImpl.security.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((request)->{
+            request.anyRequest().authenticated();
+        })
+                .csrf((request)->{
+                    HttpSecurity disable = request.disable();
+                }).
+                httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userdetailsservice()
+    {
+        UserDetails user1 = User.builder().
+                username("user1").password("{noop}user1password").roles("user").
+                build();
+
+        UserDetails user2 = User.builder().
+                username("user2").password("{noop}user2password").roles("user").
+                build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+
+
+}
--- SpringSecurity/src/main/java/com/securityImpl/security/Config/WebSecuirtyConfig.java
+++ SpringSecurity/src/main/java/com/securityImpl/security/Config/WebSecuirtyConfig.java
@@ -1,23 +1,22 @@
-package com.securityImpl.security.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecuirtyConfig {
-
-    //Defining custom filter chain to take the command
-    //back from Spring security
-    @Bean
-    public SecurityFilterChain secuirityfilterchain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request->{
-            request.anyRequest().authenticated();
-        }).httpBasic(Customizer.withDefaults());
-        return httpSecurity.build();
-    }
-}
+//package com.securityImpl.security.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class WebSecuirtyConfig {
+//
+//    //Defining custom filter chain to take the command
+//    //back from Spring security
+//    @Bean
+//    public SecurityFilterChain secuirityfilterchain(HttpSecurity httpSecurity) throws Exception {
+////        httpSecurity.authorizeHttpRequests(request->{
+////            request.anyRequest().authenticated();
+////        }).httpBasic(Customizer.withDefaults());
+//        return httpSecurity.build();
+//    }
+//}
